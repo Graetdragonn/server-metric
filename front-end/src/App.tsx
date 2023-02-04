6f2f81eae650e1c1f3f723b66a4e0bdb1b950688
Grace <gekuntz@iastate.edu>
2023-02-03 18:37:32 -0600

    Second commit after rebase main Rebasing kuntz_b1 branch

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,8 +5,9 @@ import {
 } from "react-router-dom";
 import StartPage from './pages/start/Start';
 import LoginPage from './pages/login/Login';
-import CreateAccountPage from './pages/create-account/CreateAccount';
-import DashboardPage from './pages/dashboard/Dashboard';
+import CreatePage from './pages/create-account/CreateAccount';
+import HomePage from './pages/dashboard/Dashboard';
+import SettingsPage from './pages/settings/Settings';
 import ForgotPasswordPage from "./pages/forgot-password/ForgotPassword";
 import Settings from "./pages/settings/Settings";
 import SingleServer from "./pages/single-server/SingleServer";
@@ -19,8 +20,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/createaccount" element={<CreateAccountPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/createaccount" element={<CreatePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage/>}/>
           <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/single-server" element={<SingleServer />} />
--- front-end/src/pages/settings/Settings.tsx
+++ front-end/src/pages/settings/Settings.tsx
@@ -3,10 +3,19 @@ import Header from "../../components/navigation-bar/Header";
 
 const Settings = () => {
   return (
-    <div className="Settings-Page" >
-      <Header />
-      <p className="title">Settings!</p>
+<div>
+    <Header />
+      <p className='title'> Account Settings</p>
+
+    <p className='header'>General Information:</p>
+    <p className='header2'>Username: dummyuser@gmail.com</p>
+    <p className='header2'>Password: ********</p>
+    <p className='header'>Notification Settings:</p>
+    <p className='header2'>Email: dummyuser@gmail.com</p>
+    <p className='header2'>Phone Number: 123-456-7890</p>
     </div>
+
+
   );
 }
   
