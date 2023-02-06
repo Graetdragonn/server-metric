1809d1979e007494ead70efc8690ba12a9f03064
Grace <gekuntz@iastate.edu>
2023-02-06 12:23:40 -0600

    Fixed formatting, added buttons and further functionality

--- front-end/src/pages/settings/Settings.tsx
+++ front-end/src/pages/settings/Settings.tsx
@@ -2,17 +2,21 @@ import '../../style/Master.css';
 import Header from "../../components/navigation-bar/Header";
 
 const Settings = () => {
+
   return (
 <div>
     <Header />
       <p className='title'> Account Settings</p>
 
-    <p className='header'>General Information:</p>
-    <p className='header2'>Username: dummyuser@gmail.com</p>
-    <p className='header2'>Password: ********</p>
-    <p className='header'>Notification Settings:</p>
-    <p className='header2'>Email: dummyuser@gmail.com</p>
-    <p className='header2'>Phone Number: 123-456-7890</p>
+      <p className='header'>General Information:</p>
+      <p className='header2'>Username: dummyuser@gmail.com</p>
+      <p className='header2'>Password: ********</p>
+      <p className='header'>Notification Settings:</p>
+      <p className='header2'>Email: dummyuser@gmail.com</p>
+      <p className='header2'>Phone Number: 123-456-7890</p>
+      <div>
+         <button type="submit" className="editbutton"> Edit</button>
+      </div>
     </div>
 
 
