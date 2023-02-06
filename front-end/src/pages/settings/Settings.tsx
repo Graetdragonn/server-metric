9ee3835efbd382b8a8b2d90f28ed1ba129e1136e
Grace <gekuntz@iastate.edu>
2023-02-06 12:23:40 -0600

    Fixed formatting, added buttons and further functionality

--- front-end/src/pages/settings/Settings.tsx
+++ front-end/src/pages/settings/Settings.tsx
@@ -4,19 +4,26 @@ import { useNavigate } from "react-router-dom";
 
 
 const Settings = () => {
+  const navigate = useNavigate();
+
+
   return (
     <div>
-      <div className="backbutton">
+      <div className="backbutton" onClick={() => navigate('/dashboard')}>
       back
       </div>
-      <p className='title'> Account Settings</p>
-
-    <p className='header'>General Information:</p>
-    <p className='header2'>Username: dummyuser@gmail.com</p>
-    <p className='header2'>Password: ********</p>
-    <p className='header'>Notification Settings:</p>
-    <p className='header2'>Email: dummyuser@gmail.com</p>
-    <p className='header2'>Phone Number: 123-456-7890</p>
+      <div>
+        <p className='title'> Account Settings</p>
+        <p className='header'>General Information:</p>
+        <p className='header2'>Username: dummyuser@gmail.com</p>
+        <p className='header2'>Password: ********</p>
+        <p className='header'>Notification Settings:</p>
+        <p className='header2'>Email: dummyuser@gmail.com</p>
+        <p className='header2'>Phone Number: 123-456-7890</p>
+      </div>
+      <div>
+         <button type="submit" className="editbutton"> Edit</button>
+      </div>
     </div>
 
 
