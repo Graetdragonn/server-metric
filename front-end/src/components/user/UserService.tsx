aad70993257c74682bdb0b9430c9746460b2626e
Taylor Turner <tjturner@iastate.edu>
2023-02-07 16:49:29 -0600

    post user function

--- front-end/src/components/user/UserService.tsx
+++ front-end/src/components/user/UserService.tsx
@@ -8,9 +8,8 @@ class UserService {
         return axios.get(USER_API_BASE_URL);
     }
 
-    async createUser(email: string, first: string, last: string, pass: string, user: string){
-        const response = await axios.post(USER_API_BASE_URL, { email: email, first: first, last: last, pass: pass, user: user });
-        console.log(response);
+    createUser(email: string, first: string, last: string, pass: string, user: string){
+        const response =  axios.post(USER_API_BASE_URL, { email: email, first: first, last: last, pass: pass, user: user });
         return response;
     }
 
