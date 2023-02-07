b9d3459c6e7c40ed72df030fcee0408dbbefc783
Taylor Turner <tjturner@iastate.edu>
2023-02-07 13:29:23 -0600

    Adding user service to frontend

--- front-end/src/components/user/UserService.tsx
+++ front-end/src/components/user/UserService.tsx
@@ -1,11 +1,29 @@
 import axios from "axios";
 
-const USERS_REST_API_URL = '';
+const USER_API_BASE_URL = "";
 
 class UserService {
-    getUsers() {
-        return axios.get(USERS_REST_API_URL);
+
+    getUsers(){
+        return axios.get(USER_API_BASE_URL);
     }
+
+    async createUser(email: string, first: string, last: string, pass: string, user: string){
+        const response = await axios.post(USER_API_BASE_URL, { email: email, first: first, last: last, pass: pass, user: user });
+        console.log(response);
+    }
+
+    // getEmployeeById(employeeId){
+    //     return axios.get(USER_API_BASE_URL + '/' + employeeId);
+    // }
+
+    // updateEmployee(employee, employeeId){
+    //     return axios.put(USER_API_BASE_URL + '/' + employeeId, employee);
+    // }
+
+    // deleteEmployee(employeeId){
+    //     return axios.delete(USER_API_BASE_URL + '/' + employeeId);
+    // }
 }
 
-export default new UserService();
+export default new UserService()
