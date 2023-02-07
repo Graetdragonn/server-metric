71e55ab89e249a319c11944dcf86522f72ba4a78
Grace <gekuntz@iastate.edu>
2023-02-06 19:02:52 -0600

    Further formatting to EditSettings

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,7 +8,7 @@ import LoginPage from './pages/login/Login';
 import CreatePage from './pages/create-account/CreateAccount';
 import HomePage from './pages/dashboard/Dashboard';
 import SettingsPage from './pages/settings/Settings';
-import EditSettingsPage from './pages/settings/EditSettings';
+import EditSettingsPage from './pages/edit-settings/EditSettings';
 import ForgotPasswordPage from "./pages/forgot-password/ForgotPassword";
 import Settings from "./pages/settings/Settings";
 import SingleServer from "./pages/single-server/SingleServer";
