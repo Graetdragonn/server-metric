1fe91f3cfdc63b11ad761330206fe8bb9deb346b
megan3 <megan3@iastate.edu>
2023-02-10 21:34:15 -0600

    Attempts at creating dropdown in nav bar.

--- front-end/src/components/navigation-bar/NavBar.tsx
+++ front-end/src/components/navigation-bar/NavBar.tsx
@@ -8,6 +8,18 @@ const NavBar = () => {
 
   // how to just get info without needing to do something???
 
+  const dropdownItems = [
+    {
+      address: '123'
+    }, 
+    {
+      address: '456'
+    },
+    {
+      address: '789'
+    }
+  ];
+
   var serverList: string[] = [];
 
   // list of all servers
@@ -17,9 +29,14 @@ const NavBar = () => {
   
   // stores server id that user selects from dropdown
   const [selectedServer, setSelectedServer] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleChange = (e: { target: { name: string; }; }) => {
     setSelectedServer(e.target.name);
+  };
+
+  const handleDropdownClick = () => {
+    setShowDropdown(!showDropdown);
   };
 
   return (
@@ -32,14 +49,36 @@ const NavBar = () => {
       >
         Dashboard
       </NavLink>
-      <NavLink 
+      
+{/* attempt at dropdown using onClick handler */}
+      <div className="navbar-item" onClick={handleDropdownClick}>Servers</div>
+      <div className={`dropdown-items ${showDropdown ? 'show' : ''}`}>
+        {dropdownItems.map((item, key) => (
+          <NavLink key={key} to="/single-server" state={item}>{item.address}</NavLink>
+        ))}
+      </div>
+
+{/* attempt at dropdown using basic select/option html */}
+      {/* <div className="navbar-item" aria-haspopup="menu">Servers</div>
+      <select className="dropdown">
+        <option selected={true} disabled={true}></option>
+        {dropdownItems.map((item) =>(
+          <option>
+            <NavLink to="/single-server">{item.address}</NavLink>
+          </option>
+        ))}
+      </select> */}
+
+{/* no dropdown attempt */}
+{/*   <NavLink 
         to="/single-server"
         className={({ isActive }) =>
           isActive ? 'active-navbar-item navbar-item' : 'navbar-item'
         } 
       >
         Servers
-      </NavLink>
+      </NavLink> */}
+
       <NavLink 
         to="/settings"
         className={({ isActive }) =>
--- front-end/src/pages/single-server/SingleServer.tsx
+++ front-end/src/pages/single-server/SingleServer.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import '../../style/Master.css';
 import NavBar from "../../components/navigation-bar/NavBar";
 
 const SingleServer = () => {
+
+  const location = useLocation();
+  const { state } = location;
+  console.log(state.address);
+
   return (
     <div className="Single-Server-Page">
       <NavBar />
