b7d74f0cec12911d996cfc4a5268bc83396567ac
megan3 <megan3@iastate.edu>
2023-02-13 12:32:09 -0600

    Added comments.

--- front-end/src/components/navigation-bar/Header.tsx
+++ front-end/src/components/navigation-bar/Header.tsx
@@ -1,5 +1,9 @@
 import NavBar from "./NavBar";
 
+/* 
+ * Code for NavBar was modified from https://blog.logrocket.com/how-create-multilevel-dropdown-menu-react/
+ * Order of components: Header -> NavBar -> MenuItems -> Dropdown
+ */
 const Header = () => {
     return (
         <header className="nav-area">
--- front-end/src/components/navigation-bar/MenuItems.tsx
+++ front-end/src/components/navigation-bar/MenuItems.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../../style/Master.css';
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Dropdown from './Dropdown';
 
 
