265c0b9709cc3026dc60c704903d5531652ece98
megan3 <megan3@iastate.edu>
2023-02-13 13:09:03 -0600

    Updated styling after rebase.

--- front-end/src/components/navigation-bar/MenuItems.tsx
+++ front-end/src/components/navigation-bar/MenuItems.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../../style/Master.css';
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Dropdown from './Dropdown';
 
 
@@ -12,22 +12,25 @@ interface Props {
 const MenuItems: React.FunctionComponent<Props> = ({ items, serverList }) => {
 
     const [dropdown, setDropdown] = useState(false);
+    const loc = useLocation();
+
     return (
         <li className="menu-items">
             {items.submenu ? (
                 <>
-                    <button 
-                    type="button" 
+                    <p 
                     aria-haspopup="menu" 
                     aria-expanded={dropdown ? "true" : "false"}
                     onClick={() => setDropdown(!dropdown)}
-                    className="arrow">
+                    className="arrow menu-item">
                         {items.title}{' '}
-                    </button>
+                    </p>
                     <Dropdown submenu={items.submenu} dropdown={dropdown} serverList={serverList}/>
                 </>
             ) : (
-                <NavLink to={items.url}>{items.title}</NavLink>
+                <p className='menu-item'>
+                    <NavLink to={items.url}>{items.title}</NavLink>
+                </p>
             )}
         </li>
     );
