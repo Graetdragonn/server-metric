1a1312a129df7baf4d7ed2392d88d767b5cb244d
Taylor Turner <tjturner@iastate.edu>
2023-02-14 15:13:00 -0600

    Update user type in backend

--- back-end/src/main/java/com/example/demo/User/UserType.java
+++ back-end/src/main/java/com/example/demo/User/UserType.java
@@ -2,5 +2,5 @@ package com.example.demo.User;
 
 //User types are used to determine the privileges of each user
 enum UserType {
-    CLIENT, SERVICE_PROVIDER, ADMIN, SERVER_MANAGER
+    CLIENT, SERVICE_PROVIDER, ADMIN, SERVICE_MANAGER
 }
