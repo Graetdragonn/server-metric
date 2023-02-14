730c1807bc8215c322d333d5f39e13a578da1c8b
Micah Boerjan <miboer@iastate.edu>
2023-02-14 14:30:57 -0600

    updated settings page

--- front-end/src/pages/settings/Settings.tsx
+++ front-end/src/pages/settings/Settings.tsx
@@ -1,50 +1,137 @@
 import React, {useEffect, useState } from 'react';
 import '../../style/Master.css';
 import { useNavigate } from "react-router-dom";
-import { getUserInfo } from './SettingsLogic';
+import { checkEmail, checkPassword, isEmpty, isTypeDefault, submitEdits} from './SettingsLogic';
 import UserService from '../../requests/UserService';
-import Header from "../../components/navigation-bar/Header";
-
+import { checkEmpty, emailCheck } from '../login/LoginLogic';
+import BackButton from '../../components/back-button/BackButton';
 const Settings = () => {
   const navigate = useNavigate();
-  const user = globalThis.username;
+  const user = globalThis.username; 
 
-  var [fName, setfName] = useState();
-  var [password, setpassword] = useState();
-  var [userType, setuserType] = useState();
-  var [lastName, setlastName] = useState();
+    // user input for account creation
+    const [state, setState] = useState({
+        email: "",
+        first: "",
+        last: "",
+        pass: "",
+        confirmPass: "",
+        userType: ""
+    });
 
+    const [isValidEmail, setIsValidEmail] = useState(false);
 
-  useEffect(() => {
-    async function getUserInfo(email: string) {
-      const userInfo = await UserService.getUserByEmail(email);
-      var userData = JSON.parse(userInfo);
-      setpassword(userData['userPassword']);
-      setuserType(userData['userType']);
-      setfName(userData['userFirstName']);
-      setlastName(userData['userLastName']); 
-    }
-    getUserInfo(user);
-  }, []);
+    useEffect(() => {
+      async function getUserInfo(email: string) {
+        const userInfo = await UserService.getUserByEmail(email);
+        var userData = JSON.parse(userInfo);
+        setState({...state, "email": user, "pass": userData['userPassword'],"confirmPass": userData['userPassword'], "first": userData['userFirstName'], "last": userData['userLastName'], "userType": userData['userType']});
+        console.log("Email: " + state.email);
+        console.log("Pass: " + state.pass);
+        console.log("First: " + state.first);
+        console.log("Last: " + state.last);
+        console.log("UT: " + state.userType);
+      }
+      getUserInfo(user);
+    }, []);
+
+    // tracks if user confirms password correctly
+    const [passMatch, setPassMatch] = useState(true);
+
+    // tracks if user has selcted a user type
+    const [roleSelected, setRoleSelected] = useState(true);
+
+    // checks for errors on login
+    const [error, setError] = useState(false);
+
+    // to update user information when user inputs data
+    const handleChange = (e: { target: { name: string; value: any; }; }) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // submits form
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // check that password is confirmed, show error if not
+        if (!checkPassword(state.pass, state.confirmPass)) {
+            setPassMatch(false);
+        }
+
+        // check that user type is selected, show error if not
+        if (isTypeDefault(state.userType)) {
+            setRoleSelected(false);
+        }
+
+        // verify fields and create account
+        else if (checkEmail(state.email) && checkPassword(state.pass, state.confirmPass) && !isEmpty(state.email) &&
+            !isEmpty(state.first) && !isEmpty(state.last) && !isEmpty(state.pass) &&
+            !isEmpty(state.confirmPass) && !isTypeDefault(state.userType)) {
+        }
+    };
+
+    const submitChange = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+  
+      // check that password is confirmed, show error if not
+      if (!checkPassword(state.pass, state.confirmPass)) {
+          setPassMatch(false);
+      }
+  
+      // check that user type is selected, show error if not
+      if (isTypeDefault(state.userType)) {
+          setRoleSelected(false);
+      }
+
+     
+      // verify fields and create account
+      else if (checkEmail(state.email) && checkPassword(state.pass, state.confirmPass) && !isEmpty(state.email) &&
+          !isEmpty(state.first) && !isEmpty(state.last) && !isEmpty(state.pass) &&
+          !isEmpty(state.confirmPass) && !isTypeDefault(state.userType)) {
+  
+          if (await submitEdits(state.email, state.first, state.last, state.pass, state.userType)) {
+              navigate('/settings');
+          }
+          else {
+              setError(true);
+          }
+      }
+
+      if (emailCheck(state.email) && !checkEmpty(state.email)) {
+        setIsValidEmail(true);
+      }
+  };
 
 
   return (
-    <div className="Settings-Page">
-        <Header />
-        <p className='title'> Account Settings</p>
-        <p className='header'>General Information:</p>
-        <p className='header2'>Username: {user} </p>
-        <p className='header2'>First Name: { fName } </p>
-        <p className='header2'>Last Name: {lastName}</p>
-        <p className='header2'>Password: {password}</p>
-        <p className='header'>Notification Settings:</p>
-        <p className='header2'>Email: {user}</p>
-        <div>
-          <button className="editbutton" onClick={()=> navigate('/editsettings')}> Edit</button>
-        </div>
+    <body className='Form-Body'>
+    <div>
+    <form onSubmit={submitChange} style={{display: isValidEmail ? 'none' : ''}}>
+    <BackButton></BackButton>
+      <h1>Settings</h1>
+      <label>First Name</label>
+      <input type="text" name="first" required={true} value={state.first} onChange={handleChange}></input>
+      <br></br>
+      <label>Last Name</label>
+      <input type="text" name="last" required={true} value={state.last} onChange={handleChange}></input>
+      <br></br>
+      <label>Password</label>
+      <input name="pass" required={true} value={state.pass} onChange={handleChange}></input>
+      <br></br>
+      <label>Confirm Password</label>
+      <input name="confirmPass" required={true} value={state.confirmPass} onChange={handleChange}></input>
+      <br></br>
+      <button >Submit</button>
+    </form>
+    <form style={{display: isValidEmail ? '' : 'none'}}>
+    <p style={{fontSize:50, textAlign:'center'}}>Updated settings were saved to account</p>
+    <button onClick={() => navigate('/Dashboard')}>Dashboard</button>
+    </form>
     </div>
-
-
+    </body>
   );
 }
   
--- front-end/src/pages/settings/SettingsLogic.tsx
+++ front-end/src/pages/settings/SettingsLogic.tsx
@@ -1,4 +1,3 @@
-import { isEmpty, checkEmail } from "../create-account/CreateAccountLogic";
 import UserService from '../../requests/UserService';
 var password;
 
@@ -10,11 +9,65 @@ export async function getUserInfo(email: string) {
     var userType = userData['userType'];
     var firstName = userData['userFirstName'];
     var lastName = userData['userLastName'];
+}
 
-    //test alerts
-    //alert(email);
-    //alert(password);
-    //alert(userType);
-    //alert(firstName);
-    //alert(lastName);
+/**
+ * Checks that user confirmed password correctly
+ * @param pass        User password
+ * @param confirmPass User confirm password
+ * @returns true if passwords match, false otherwise
+ */
+export function checkPassword(pass: string, confirmPass: string): boolean {
+    if (pass === confirmPass) {
+
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Checks to make sure no field is empty
+ * @param s string in input field
+ * @returns true if field is empty, false otherwise
+ */
+export function isEmpty(s: string): boolean {
+    if (s === "") {
+        alert("Field cannot be left empty");
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Checks if user has selected user type
+ * @param s User type selected
+ * @returns true if user has not selected, false otherwise
+ */
+export function isTypeDefault(s: string): boolean {
+    if (s === "default" || s === "") {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Checks email format
+ * @param email User email
+ * @returns true if is email format, false otherwise
+ */
+export function checkEmail(email: string): boolean {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email)) {
+        return true;
+    }
+    return false;
+}
+
+export async function submitEdits(email: string, first: string, last: string, pass: string, user: string) {
+
+    const res = await UserService.updateUser(email, first, last, pass, user);
+    if (res === "") {
+        return false;
+    }
+    return true;
 }
