d2436e535b7e375bb90ec014d15eb79906313db1
megan3 <megan3@iastate.edu>
2023-02-13 23:39:47 -0600

    Added request for getting traffic data.

--- front-end/src/pages/dashboard/Dashboard.tsx
+++ front-end/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import '../../style/Master.css'
 import Header from "../../components/navigation-bar/Header";
 import { useNavigate } from 'react-router-dom';
+import { getNumPacketsSentPerAddresses } from './DashboardLogic';
+import { getServersByUser } from '../../components/navigation-bar/NavBarLogic';
 
 const DashboardPage = () => {
   const navigate = useNavigate();
+
+  // dictionary of packets sent per each address
+  const [packetsPerIp, setPacketsPerIp] = useState(new Map<string, number>());
+  const [userAddresses, setUserAddresses] = useState([] as string[]);
+
+  // get data to render on screen immediately
+  useEffect(() => {
+    const getUserAddresses = async () => {
+      setUserAddresses(await getServersByUser(username));
+    };
+    getUserAddresses();
+
+    const getPacketsPerIp = async () => {
+      setPacketsPerIp(await getNumPacketsSentPerAddresses(userAddresses));
+    };
+    getPacketsPerIp();    
+  }, []);
+
   return (
     <div className="Dashboard-Page">
       <Header />
@@ -11,6 +32,6 @@ const DashboardPage = () => {
       <button className='addServer' onClick={() => navigate('/addserver')}>Add Server</button>
     </div>
   );
-}
+};
   
 export default DashboardPage;
