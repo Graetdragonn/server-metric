78f9adba1cab15e5883ab39db970efcebff92648
Micah Boerjan <miboer@iastate.edu>
2023-02-15 16:02:10 -0600

    added CSS

--- front-end/src/pages/dashboard/Dashboard.tsx
+++ front-end/src/pages/dashboard/Dashboard.tsx
@@ -27,9 +27,6 @@ const DashboardPage = () => {
     getUserAddresses();
   }, [packetsPerIp]);
 
-
-  
-
   const renderPacketsPerAddress = () => {
     // if (packetsPerIp.size) {
     //   // forEach(value, key)
@@ -46,7 +43,7 @@ const DashboardPage = () => {
     //   console.log("No addresses found.");
     //   return <p>No addresses found.</p>
     // }
-    return packetsPerIp.map((addr) => <p>Address {addr.address} has sent {addr.numPackets} packet(s)</p>);
+    return packetsPerIp.map((addr) => <div className='div-for-single-address'>Address {addr.address} has sent {addr.numPackets} packet(s)</div>);
   };
 
   const renderNoAddresses = () => {
@@ -56,10 +53,22 @@ const DashboardPage = () => {
   return (
     <div className="Dashboard-Page">
       <Header />
-      <button className='addServer' onClick={() => navigate('/addserver')}>Add Server</button>
+     
       <div>
-        {packetsPerIp.length > 0 && renderPacketsPerAddress()}
-        {packetsPerIp.length < 1 && renderNoAddresses()}
+        <br></br>
+          <div className='div-for-addresses '>
+            <h1>Server Packet Traffic</h1>
+            {packetsPerIp.length > 0 && renderPacketsPerAddress()}
+            {packetsPerIp.length < 1 && renderNoAddresses()}
+        </div>
+        <br></br>
+        <div className='div-for-addresses'>
+          <h1>Server Settings</h1>
+        <button onClick={() => navigate('/addserver')}>Add Server</button>
+       
+        </div>
+          
+        
       </div>
     </div>
   );
