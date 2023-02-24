465382f73edf29a936d9fb046380633b47232ccf
Grace <gekuntz@iastate.edu>
2023-02-23 19:29:04 -0600

    Initial graph setup with D3

--- front-end/src/components/graph/GraphLogic.tsx
+++ front-end/src/components/graph/GraphLogic.tsx
@@ -9,12 +9,7 @@ type dataProps = {
     data: number[];
 }
 
-
-
-
 const tempFakeData = [3,1,3,2,2,5,2,1,3]
-
-
 
 export const HistogramGraph = ({width, height, data}: dataProps)=> {
     const bucketGen = d3
