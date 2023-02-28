2853240c37e94e03bc4b776299b0c17f6c22730b
Micah Boerjan <miboer@iastate.edu>
2023-02-28 10:44:16 -0600

    stupid fix

--- back-end/src/main/java/com/example/demo/Authentication/AuthenticationController.java
+++ back-end/src/main/java/com/example/demo/Authentication/AuthenticationController.java
@@ -2,10 +2,7 @@ package com.example.demo.Authentication;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -14,11 +11,13 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     @PostMapping("/register")
+    @CrossOrigin
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
      return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/sign-in")
+    @CrossOrigin
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
