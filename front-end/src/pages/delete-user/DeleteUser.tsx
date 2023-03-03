21f98643c5ce69d6ee6d6867b9e2a89d209c60c7
Taylor Turner <tjturner@iastate.edu>
2023-03-03 09:24:22 -0600

    Changed to dropdown

--- front-end/src/pages/delete-user/DeleteUser.tsx
+++ front-end/src/pages/delete-user/DeleteUser.tsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BackButton from "../../components/back-button/BackButton";
 import Header from "../../components/navigation-bar/Header";
+import { getAllUsers } from "../../components/user-list/UserListLogic";
 import { emailCheck, checkEmpty, getUserType } from "../login/LoginLogic";
 import { deleteServerProviderClientByEmail, deleteUser, getClientServiceProvider } from "./DeleteUserLogic";
 
 
 const DeleteUserPage = () => {
-    // for screen navigation
+  // for screen navigation
   const navigate = useNavigate();
 
   // user input for email
   const [email, setEmail] = useState("");
+
+  // all users
+  const [userList, setUserList] = useState([] as any[]);
 
   const [isValidEmail, setIsValidEmail] = useState(false);
 
@@ -29,6 +33,13 @@ const DeleteUserPage = () => {
     }
   }
   getSP();
+
+  // get all servers
+  const getUserList = async () => {
+    var users = await getAllUsers();
+    setUserList(users);
+  }
+  getUserList();
 
   // to update user information when user inputs data
   const handleChange = (e: { target: { name: string; value: any; }; }) => {
@@ -51,31 +62,35 @@ const DeleteUserPage = () => {
         }
         else {
             setError(true);
-        }
+      }
     }
-    
+
   };
 
   return (
-<div className='Forgot-Password-Page'>
-    <Header></Header>
-    <body className='Form-Body'>
-    <div>
-    <form onSubmit={handleSubmit}  style={{display: isValidEmail ? 'none' : ''}}>
-    <BackButton></BackButton>
-      <h1>Delete a User</h1>
-      <p style={{fontSize:15, textAlign:'center'}}>Please enter the email of the user to delete</p>
-      <input placeholder='Email' type="text" required={true} name="email" value={email} onChange={handleChange}></input>
-      <button>Submit</button>
-      <br></br>
-      <span style={{ visibility: error ? 'visible' : 'hidden' }} className='error'>User does not exist</span>
-    </form>
-    <form onSubmit={handleSubmit} style={{display: isValidEmail ? '' : 'none'}}>
-    <p style={{fontSize:20, textAlign:'center'}}>User {email} was successfully deleted</p>
-    <button onClick={() => navigate('/dashboard')}>Back to dashboard</button>
-    </form>
-    </div>
-    </body>
+    <div className='Forgot-Password-Page'>
+      <Header></Header>
+      <body className='Form-Body'>
+        <div>
+          <form onSubmit={handleSubmit} style={{ display: isValidEmail ? 'none' : '' }}>
+            <BackButton></BackButton>
+            <h1>Delete a User</h1>
+            <div className="center">
+              <select onChange={handleChange}>
+                <option value="default"> - Select a User to Delete -</option>
+                {userList.map(user => { return <option value={user.userEmail}>{user.userEmail}</option>; })}
+              </select>
+            </div>
+            <button>Submit</button>
+            <br></br>
+            <span style={{ visibility: error ? 'visible' : 'hidden' }} className='error'>Please select a user</span>
+          </form>
+          <form onSubmit={handleSubmit} style={{ display: isValidEmail ? '' : 'none' }}>
+            <p style={{ fontSize: 20, textAlign: 'center' }}>User {email} was successfully deleted</p>
+            <button onClick={() => navigate('/dashboard')}>Back to dashboard</button>
+          </form>
+        </div>
+      </body>
     </div>
   );
 }
