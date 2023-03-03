7c2f258b941b326fb4b708f43ada59ea5f17db0a
Micah Boerjan <miboer@iastate.edu>
2023-03-02 20:22:49 -0600

    Added Stuff for JWT security

--- back-end/src/main/java/com/example/demo/Security/CorsFilter.java
+++ back-end/src/main/java/com/example/demo/Security/CorsFilter.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 
 public class CorsFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig){
 
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
