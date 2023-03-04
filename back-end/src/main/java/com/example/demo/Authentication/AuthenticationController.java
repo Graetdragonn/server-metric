6e77e4db733f0ed4859f2670b3eb957d9b673fe3
Micah Boerjan <miboer@iastate.edu>
2023-03-04 12:34:33 -0600

    add CrossOrigin on all controllers and changed authentication routing

--- back-end/src/main/java/com/example/demo/Authentication/AuthenticationController.java
+++ back-end/src/main/java/com/example/demo/Authentication/AuthenticationController.java
@@ -2,23 +2,23 @@ package com.example.demo.Authentication;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
-    @PostMapping("/register")
+    @PostMapping("register")
+    @CrossOrigin
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
      return ResponseEntity.ok(authenticationService.register(request));
     }
 
-    @PostMapping("/sign-in")
+    @PostMapping("sign-in")
+    @CrossOrigin
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
