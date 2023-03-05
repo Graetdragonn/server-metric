de2ce83501dfa10a3516dafd6865900bb6215521
Grace <gekuntz@iastate.edu>
2023-03-05 16:56:50 -0600

    Added buckets and dimensions

--- front-end/src/components/graph/GraphLogic.tsx
+++ front-end/src/components/graph/GraphLogic.tsx
@@ -10,12 +10,9 @@ type dataProps = {
     data: number[];
 }
 
-
 const tempFakeData = [3,1,3,2,2,5,2,1,3]
 
 export const HistogramGraph = ({width, height, data}: dataProps)=> {
-    const bucketGen = d3
-        .bin()
         .value((d) => d)
         .domain([0,5])
         .thresholds([0,1,2,3,4,5]);
