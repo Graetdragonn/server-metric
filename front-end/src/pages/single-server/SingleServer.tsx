8ac9871f7b0e5a81a717d8635bfa9ed501ce5814
Micah Boerjan <miboer@iastate.edu>
2023-03-08 08:43:25 -0600

    added received packets into single server

--- front-end/src/pages/single-server/SingleServer.tsx
+++ front-end/src/pages/single-server/SingleServer.tsx
@@ -2,29 +2,35 @@ import { useLocation } from "react-router-dom";
 import '../../style/Master.css';
 import Header from "../../components/navigation-bar/Header";
 import { useEffect, useState } from "react";
-import { getServerTraffic } from "./SingleServerLogic";
+import {getReceivedServerTraffic, getSentServerTraffic} from "./SingleServerLogic";
 
 const SingleServer = () => {
 
   const location = useLocation();
   const { state } = location;
-  const [trafficList, setTrafficList] = useState([] as any[]);
-  var traffic: string[];
+  const [sentTrafficList, setSentTrafficList] = useState([] as any[]);
+  const [receivedTrafficList, setReceivedTrafficList] = useState([] as any[]);
+  var sentTraffic: string[];
+  var receivedTraffic: string[];
 
   useEffect(() => {
     async function getTraffic() {
-      traffic = await getServerTraffic(state);
-      setTrafficList(traffic);
+      sentTraffic = await getSentServerTraffic(state);
+      setSentTrafficList(sentTraffic);
+      receivedTraffic = await getReceivedServerTraffic(state);
+      setReceivedTrafficList(receivedTraffic)
     }
     getTraffic();
-  }, [trafficList]);
 
-  const renderTraffic = () => {
-    return trafficList.map((event) => 
+  }, [sentTrafficList, receivedTrafficList]);
+
+  const renderSentTraffic = () => {
+    return sentTrafficList.map((event) =>
 
       <div className='div-for-single-address'>
         Date: {event.time.month}/{event.time.day}/{event.time.year} <br></br>
         Time: {event.time.hour}:{event.time.min}:{event.time.ms} <br></br>
+          Source IP: {event.srcIP} <br></br>
         Destination IP: {event.dstIP} <br></br>
         Source Port: {event.srcPort} <br></br>
         Destination Port: {event.dstPort}
@@ -32,6 +38,21 @@ const SingleServer = () => {
    
     );
   }
+
+    const renderReceivedTraffic = () => {
+        return receivedTrafficList.map((event) =>
+
+            <div className='div-for-single-address'>
+                Date: {event.time.month}/{event.time.day}/{event.time.year} <br></br>
+                Time: {event.time.hour}:{event.time.min}:{event.time.ms} <br></br>
+                Source IP: {event.srcIP} <br></br>
+                Destination IP: {event.dstIP} <br></br>
+                Source Port: {event.srcPort} <br></br>
+                Destination Port: {event.dstPort}
+            </div>
+
+        );
+    }
 
   const noTraffic = () => {
     return <p>No network traffic found for this server</p>
@@ -41,17 +62,17 @@ const SingleServer = () => {
     <div className="Single-Server-Page">
       <Header />
         <div>
-      <h1 className='Gradient-Text' style={{textAlign: "center"}}> Network Traffic for Server {state}</h1>
-        <div className="background-side-by-side-parent">
-      <div className='background-side-by-side-first-child' style={{maxWidth: 700}}>
+            <h1 className='Gradient-Text' style={{fontWeight: 900, textAlign: "center"}}> Network Traffic for Server {state}</h1>
+            <div className="background-side-by-side-parent">
+                <div className='background-side-by-side-first-child' style={{maxWidth: 700}}>
         <h1>Sent Packets</h1>
-        {trafficList.length > 0 && renderTraffic()}
-        {trafficList.length < 1 && noTraffic()}
+        {sentTrafficList.length > 0 && renderSentTraffic()}
+        {sentTrafficList.length < 1 && noTraffic()}
       </div>
         <div className='background-side-by-side-child' style={{maxWidth: 700}}>
             <h1>Received Packets</h1>
-            {trafficList.length > 0 && renderTraffic()}
-            {trafficList.length < 1 && noTraffic()}
+            {sentTrafficList.length > 0 && renderReceivedTraffic()}
+            {sentTrafficList.length < 1 && noTraffic()}
         </div>
         </div>
         </div>
