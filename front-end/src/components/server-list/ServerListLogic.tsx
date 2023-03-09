ec79548dc7f587262bb2883c5917e8c2bc0306df
megan3 <megan3@iastate.edu>
2023-03-08 21:36:22 -0600

    Add idea of logic for new server list on left side of dashboard.

--- front-end/src/components/server-list/ServerListLogic.tsx
+++ front-end/src/components/server-list/ServerListLogic.tsx
@@ -19,3 +19,16 @@ export async function getServerInfo(server: string){
     var serverData = JSON.parse(res);
     return serverData;
 }
+
+/**
+ * Get all servers that belong to a certain service provider
+ * @returns all servers in database belonging to a specified service provider
+ */
+export async function getClientsByProvider(email: string){
+    // TODO: will want to call getUserByEmail()
+    // then return list of clients (see clients key in json)
+
+    //var res = await ServerService.getAllServers();
+    //return JSON.parse(res);
+    return [];
+}
