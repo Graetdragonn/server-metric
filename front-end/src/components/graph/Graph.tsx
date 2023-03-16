1a24324ed412978a410cc0cdce3de4d947cda858
Grace <gekuntz@iastate.edu>
2023-03-16 17:52:28 -0500

    Testing push after rebase

--- front-end/src/components/graph/Graph.tsx
+++ front-end/src/components/graph/Graph.tsx
@@ -4,7 +4,6 @@ import { HistogramGraph } from "./GraphLogic";
 
 const tempFakeData = [1,2,3,3,5,4,6,7,8,5,6,8,9]
 
-
 const Graph = ({width = 700, height = 400}) => {
   return (
     <div style={{marginTop: 30, marginBottom: 20}}>
