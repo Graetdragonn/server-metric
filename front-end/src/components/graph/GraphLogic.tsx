67700ce86de9094a5b36f34b7efa7ce7dd43aa24
Grace <gekuntz@iastate.edu>
2023-03-19 17:49:52 -0500

    Initial graph label setup

--- front-end/src/components/graph/GraphLogic.tsx
+++ front-end/src/components/graph/GraphLogic.tsx
@@ -11,24 +11,25 @@ type dataProps = {
 }
 
 const GRAPH_MARGINS = {top: 20, bottom: 20, left: 30, right:30};
+const NUM_BUCKETS = 5;
 
 
 export const HistogramGraph = ({width, height, data}: dataProps)=> {
-    const axesReference = useRef(null);
-    const axesHeight = height - GRAPH_MARGINS.top - GRAPH_MARGINS.bottom;
-    const axesWidth = width - GRAPH_MARGINS.right - GRAPH_MARGINS.left;
+    const axisReference = useRef(null);
+    const axisHeight = height - GRAPH_MARGINS.top - GRAPH_MARGINS.bottom;
+    const axisWidth = width - GRAPH_MARGINS.right - GRAPH_MARGINS.left;
 
     const xDimension = d3
         .scaleLinear()
         .domain([0,25])
-        .range([25, axesWidth]);
+        .range([25, axisWidth]);
 
     const buckets = useMemo(()=> {
         const bucketGen = d3
             .bin()
             .value((d) => d)
             .domain([0,25])
-            .thresholds([0,5,10,15,20,25]);
+            .thresholds(xDimension.ticks(NUM_BUCKETS));
         return bucketGen(data);
     }, [xDimension]);
 
@@ -36,7 +37,7 @@ export const HistogramGraph = ({width, height, data}: dataProps)=> {
     const yDimension = useMemo(() =>{
         const maxHeight = Math.max(...buckets.map((bucket)=> bucket?.length));
         return d3.scaleLinear()
-            .range([height, 0])
+            .range([axisHeight, 0])
             .domain([0, maxHeight]);            
     }, [data, height]);
 
@@ -49,16 +50,30 @@ export const HistogramGraph = ({width, height, data}: dataProps)=> {
                 x = {xDimension(bucket.x0 as number) + 2}
                 width = {xDimension(bucket.x1 as number) - xDimension(bucket.x0 as number) - 2}
                 y = {yDimension(bucket.length)}
-                height = {height - yDimension(bucket.length)}
+                height = {axisHeight - yDimension(bucket.length)}
             />
         );
     });
 
+    //Rendering X and Y axis.
+    useEffect(() =>{
+        const svg = d3.select(axisReference.current);
+        //svg.selectAll("*").remove();
+
+        const make_X_Axis = d3.axisBottom(xDimension);
+        svg.append("g").attr("transform", "translate(0," + axisHeight + ")").call(make_X_Axis);
+
+        const make_Y_Axis = d3.axisLeft(yDimension);
+        svg.append("g").call(make_Y_Axis);
+    }, [xDimension, yDimension, axisHeight]);
+
     return(
-        <div>
-            <svg width={width} height={height}>
+        <svg width={width} height={height}>
+            <g width={axisWidth} height={axisHeight} transform={`translate(${[GRAPH_MARGINS.left, GRAPH_MARGINS.top].join(",")})`}>
                 {makeRectangles}
-            </svg>
-        </div>
+            </g>
+            <g width={axisWidth} height={axisHeight} ref={axisReference} transform={`translate(${[GRAPH_MARGINS.left, GRAPH_MARGINS.top].join(",")})`}>
+            </g>
+        </svg>
     );
 }
