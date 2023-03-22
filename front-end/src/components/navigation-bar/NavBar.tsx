edb784562534c7ee74e5e63fa2fa0332ddaa8edd
megan3 <megan3@iastate.edu>
2023-03-22 12:15:34 -0500

    Delete unnecessary code in NavBar.

--- front-end/src/components/navigation-bar/NavBar.tsx
+++ front-end/src/components/navigation-bar/NavBar.tsx
@@ -1,30 +1,7 @@
-import { useEffect, useState } from 'react';
 import '../../style/Master.css';
-import { getServersByUser } from './NavBarLogic';
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-
-  // list of server ip addresses
-  const [serverListObj, setServerListObj] = useState([] as ({ title: string; url: string;}[]));
-
-  // get data to render on screen immediately (specifically list of servers for dropdown)
-  useEffect(() => {
-    var serverList = [] as string[];
-    const getServerList = async () => {
-      const email = JSON.parse(localStorage.getItem('email') || '');
-      serverList = await getServersByUser(email);
-      serverList.forEach((item) => {
-        handleAddNewServer(item);
-      });
-    };
-    getServerList();    
-  }, []);
-
-  // handler for adding a new server ip address to the list
-  const handleAddNewServer = (address: string) => {
-    setServerListObj(previousList => ([...previousList, {title: address, url: '/single-server'}]));
-  };
 
   // ideally, put this somewhere else like in a global file then import
   const menuItems = [
@@ -47,7 +24,7 @@ const NavBar = () => {
       <ul className="menus">
         {menuItems.map((menu, index) => {
           return (        
-            <li className="menu-items">
+            <li className="menu-items" key={index}>
               <p>
                 <NavLink to={menu.url}>{menu.title}</NavLink>
               </p>
