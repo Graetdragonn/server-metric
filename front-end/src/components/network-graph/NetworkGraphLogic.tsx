48f5ee52851304dd6553233255628322a6065991
Micah Boerjan <miboer@iastate.edu>
2023-04-05 16:33:15 -0500

    got a network graph working still messing with styling

---
+++ front-end/src/components/network-graph/NetworkGraphLogic.tsx
@@ -0,0 +1,93 @@
+import TrafficService from "../../requests/TrafficService";
+
+function addressInNodeList(address: string, addresses: [{id: number; label: string}]){
+    for(let i = 0; i < addresses.length; i++){
+        // @ts-ignore
+        if(address == addresses[i]["label"]){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function addressInUserAddress(address: string, addresses: string[]){
+    for(let i = 0; i < addresses.length; i++){
+        // @ts-ignore
+        if(address == addresses[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
+export async function generateNodesForNetworkGraph(userAddresses: string[]){
+    const res = await TrafficService.getAllTraffic();
+    const trafficList = JSON.parse(res);
+    let lengthOfUserAddresses = userAddresses.length;
+    let lengthOfTrafficList = trafficList.length;
+    // @ts-ignore
+    let nodeList: [{id: number; label: string, color: string}] = [];
+
+    for(let i = 0; i < lengthOfUserAddresses; i++){
+        nodeList.push({id: i, label: userAddresses[i], color: " #ee8434"})
+
+    }
+
+
+    for(let i = 0; i <lengthOfTrafficList; i++){
+
+        const addrRec = trafficList[i]["dstIP"];
+        const addrSnt = trafficList[i]["srcIP"];
+        if(i <= 100){
+            if(addressInUserAddress(addrRec, userAddresses)){
+
+                if(!addressInNodeList(addrSnt, nodeList)){
+
+                    nodeList.push({id: i + lengthOfUserAddresses, label: addrSnt, color: "#94579E"})
+                }
+            }else if(addressInUserAddress(addrSnt, userAddresses)){
+
+                if(!addressInNodeList(addrRec, nodeList)){
+
+                    nodeList.push({id: i + lengthOfUserAddresses, label: addrRec, color: "#94579E"})
+                }
+            }
+        }
+
+    }
+
+    return nodeList
+}
+
+function getIDFromNodeList(address: string, nodeList: any[]){
+    for(let i = 0; i < nodeList.length; i++){
+        if(nodeList[i]["label"] == address){
+            return nodeList[i]["id"];
+        }
+    }
+    return null;
+}
+
+export async function generateEdgesForNetworkGraph(nodeList: any[]){
+    const res = await TrafficService.getAllTraffic();
+    const trafficList = JSON.parse(res);
+    const edgeList = [] as any[]
+
+
+    for(let i = 0; i < nodeList.length; i++){
+        let addressToFindConnection = nodeList[i]["label"];
+        for(let j = 0; j < trafficList.length; j++){
+
+            const addrRec = trafficList[j]["dstIP"];
+            const addrSnt = trafficList[j]["srcIP"];
+            if(addrRec == addressToFindConnection){
+                edgeList.push({from: getIDFromNodeList(addrSnt, nodeList), to: nodeList[i]["id"]})
+            }else if(addrSnt == addressToFindConnection){
+                edgeList.push({from: nodeList[i]["id"], to: getIDFromNodeList(addrRec, nodeList)})
+            }
+        }
+    }
+
+    return edgeList;
+}
