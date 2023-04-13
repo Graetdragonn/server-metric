79ddcb649dc33eac12f8d9269c005ed18d99c3f9
Micah Boerjan <miboer@iastate.edu>
2023-04-13 13:40:16 -0500

    fixed graphs in service provider and clients and made them collapsable

--- front-end/src/components/packet-per-ip-graph-client/PacketPerIPClient.tsx
+++ front-end/src/components/packet-per-ip-graph-client/PacketPerIPClient.tsx
@@ -4,11 +4,17 @@ import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recha
 import {getNumPacketsSentAndReceivedClient, getServersByUser} from "./PacketPerIPClientLogic";
 
 
-export default function PacketPerIPClient() {
+interface PacketPerIPClientComponentProps {
+    subnetAddress: string;
+}
+
+export default function PacketPerIPClient({subnetAddress}: PacketPerIPClientComponentProps) {
     // track sent and received packets per IP - client
     const [allPacketsPerIpClient, setAllPacketsPerIpClient] = useState([] as any[]);
 
     let userInfo: string[]; // user info
+
+    const navigate = useNavigate(); // for screen navigation
 
     // sent and received packets per client
     let allPacketsPerClient: any[];
@@ -17,25 +23,40 @@ export default function PacketPerIPClient() {
         async function getData() {
             const email = localStorage.getItem("email")!.substring(1, localStorage.getItem("email")!.length - 1);
             userInfo = await getServersByUser(email);
-            allPacketsPerClient = await getNumPacketsSentAndReceivedClient(userInfo);
+            allPacketsPerClient = await getNumPacketsSentAndReceivedClient(userInfo, subnetAddress);
             setAllPacketsPerIpClient(allPacketsPerClient);
         }
         getData()
     }, [])
 
 
-    const navigate = useNavigate(); // for screen navigation
+
+
+    function getGraphName(){
+        return "Servers on Subnet: " + subnetAddress
+    }
 
     function render(){
-        return <BarChart height={500} width={1400} data={allPacketsPerIpClient}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="address" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar onClick={(data) => { navigate("/single-server", { state: data.address }) }} name="Number of Packets Sent" barSize={30} dataKey="numPacketsSent" fill= "var(--orange_wheel)" />
-            <Bar onClick={(data) => { navigate("/single-server", { state: data.address }) }} name="Number of Packets Received" barSize={30} dataKey="numPacketsReceived" fill= "var(--some_purple)" />
-        </BarChart>
+        return <>
+            <button type="button" className="collapsible">{getGraphName()}</button>
+            <div className="content">
+            <BarChart height={500} width={1400} data={allPacketsPerIpClient}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="address"/>
+                <YAxis/>
+                <Tooltip/>
+                <Legend/>
+                <Bar onClick={(data) => {
+                    navigate("/single-server", {state: data.address});
+                }} name="Number of Packets Sent" barSize={30} dataKey="numPacketsSent" fill="var(--orange_wheel)"/>
+                <Bar onClick={(data) => {
+                    navigate("/single-server", {state: data.address});
+                }} name="Number of Packets Received" barSize={30} dataKey="numPacketsReceived"
+                     fill="var(--some_purple)"/>
+            </BarChart>
+            </div>
+            </>
+
     }
 
     return (
--- front-end/src/pages/dashboard/Dashboard.tsx
+++ front-end/src/pages/dashboard/Dashboard.tsx
@@ -7,6 +7,8 @@ import PacketPerIPClient from "../../components/packet-per-ip-graph-client/Packe
 import PacketPerIPSP from "../../components/packet-per-ip-graph-sp/PacketPerIPSP";
 import orangeCircle from './icons8-orange-circle-48.png'
 import purpleCircle from './icons8-purple-circle-48.png'
+import {getClientAndSubnetServersByUser, getSubnetServersByUser} from "./DashboardLogic";
+import {useEffect, useState} from "react";
 
 /**
  * Render dashboard page for each user type
@@ -15,6 +17,70 @@ import purpleCircle from './icons8-purple-circle-48.png'
 export default function DashboardPage() {
   const navigate = useNavigate(); // for screen navigation
   const userType = localStorage.getItem('userType'); // get user's type
+    const [clientSubnetListState, setClientSubnetListState] = useState([] as any[]);
+    const [spClientAndSubnetListState, setSPClientAndSubnetListState] = useState([] as any[]);
+    let clientSubnetServers: string[]
+    let clientAndSubnetList: any[]
+
+
+
+    useEffect(() => {
+
+        async function getData() {
+            const email = localStorage.getItem("email")!.substring(1, localStorage.getItem("email")!.length - 1);
+            clientSubnetServers = await getSubnetServersByUser(email)
+            setClientSubnetListState(clientSubnetServers);
+            clientAndSubnetList = await getClientAndSubnetServersByUser(email)
+            setSPClientAndSubnetListState(clientAndSubnetList)
+
+        }
+        getData()
+    }, [])
+
+
+    function renderClientGraphs(subnetList: string[]){
+        let returning = [];
+        if(subnetList.length > 0 ){
+            for(let i = 0; i<subnetList.length; i++){
+                returning.push(<PacketPerIPClient subnetAddress={subnetList[i]}></PacketPerIPClient>)
+            }
+            return returning;
+        }else{
+            return [];
+        }
+    }
+
+    function renderSPGraphs(clientAndListOfSubnetList: any[]){
+        let returning = [];
+        if(clientAndListOfSubnetList.length > 0 ){
+            for(let i = 0; i<clientAndListOfSubnetList.length; i++){
+                for(let j = 0; j<clientAndListOfSubnetList[i]["subnets"].length; j++){
+                    returning.push(<PacketPerIPSP clientFullName={clientAndListOfSubnetList[i]["clientName"]} clientEmail={clientAndListOfSubnetList[i]["clientEmail"]} subnetAddress={clientAndListOfSubnetList[i]["subnets"][j]}></PacketPerIPSP>)
+                }
+            }
+            return returning;
+        }else{
+            return [];
+        }
+    }
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            // @ts-ignore
+            this.classList.toggle("active");
+            // @ts-ignore
+            var content = this.nextElementSibling;
+            if (!content.style.display || content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
 
   return (
     <div className="Dashboard-Page">
@@ -35,20 +101,22 @@ export default function DashboardPage() {
       <br />
 
       <div className="white-div" style={{ width: 1400, display: userType !== "CLIENT" ? 'none' : '' }}>
-          <h3 style={{display: "inline-flex", textAlign: "center", marginLeft: "33%",  textDecoration: "underline" }}> Graph of Packets Sent and Received through each Server</h3>
-          <PacketPerIPClient></PacketPerIPClient>
-          <br />
-          <br/>
           <h3 style={{display: "inline-flex", textAlign: "center", marginLeft: "38%",  textDecoration: "underline" }}> Graph of Overall Network Connections</h3>
           <div  style={{backgroundColor: "white", marginLeft: "4%", height: "510px", width: "1410 px", borderColor: "black", borderStyle: "solid"}}>
-            <NetworkGraph></NetworkGraph>
+              <NetworkGraph></NetworkGraph>
           </div>
           <p style = {{display: "inline-flex", textAlign: "center", marginLeft: "39%"}}> <img style={{ width:20, height: 20}} src={orangeCircle} alt="Logo" /> <span style={{color:"var(--orange_wheel"}}>: Local Addresses &nbsp;</span> <img style={{ width:20, height: 20}} src={purpleCircle} alt="Logo" /> <span style={{color:"var(--some_purple"}}>: Global Addresses</span></p>
+          <br/>
+          <br/>
+          <h3 style={{display: "inline-flex", textAlign: "center", marginLeft: "33%",  textDecoration: "underline" }}> Graph of Packets Sent and Received through each Server</h3>
+          {renderClientGraphs(clientSubnetListState)}
+
+
       </div>
 
       <div className="white-div" style={{ width: 1400, display: userType !== "SERVICE_PROVIDER" ? 'none' : '' }}>
-          <h3 style={{display: "inline-flex", textAlign: "center", marginLeft: "31%",  textDecoration: "underline" }}> Graph of Packets Sent/Received through Different Client Servers</h3>
-          <PacketPerIPSP></PacketPerIPSP>
+          <h3 style={{display: "inline-flex", textAlign: "center", marginLeft: "29%",  textDecoration: "underline" }}> Graphs of Packets Sent/Received through Different Client Servers</h3>
+          {renderSPGraphs(spClientAndSubnetListState)}
           <br />
       </div>
 
