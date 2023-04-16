c34a38fd1aabdf24cb3b2656e0f0e5badd7adfb6
Micah Boerjan <miboer@iastate.edu>
2023-04-16 15:37:51 -0500

    fixed graphs in service provider and clients and made them collapsable

--- front-end/src/components/packet-per-ip-graph-client/PacketPerIPClient.tsx
+++ front-end/src/components/packet-per-ip-graph-client/PacketPerIPClient.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
+import {Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 import {getNumPacketsSentAndReceivedClient, getServersByUser} from "./PacketPerIPClientLogic";
+import Collapsible from "react-collapsible";
 
 
 interface PacketPerIPClientComponentProps {
@@ -11,7 +12,7 @@ interface PacketPerIPClientComponentProps {
 export default function PacketPerIPClient({subnetAddress}: PacketPerIPClientComponentProps) {
     // track sent and received packets per IP - client
     const [allPacketsPerIpClient, setAllPacketsPerIpClient] = useState([] as any[]);
-
+    let [posData, setPosData] = useState({} as any);
     let userInfo: string[]; // user info
 
     const navigate = useNavigate(); // for screen navigation
@@ -30,32 +31,38 @@ export default function PacketPerIPClient({subnetAddress}: PacketPerIPClientComp
     }, [])
 
 
-
-
     function getGraphName(){
         return "Servers on Subnet: " + subnetAddress
     }
 
     function render(){
+
         return <>
-            <button type="button" className="collapsible">{getGraphName()}</button>
-            <div className="content">
-            <BarChart height={500} width={1400} data={allPacketsPerIpClient}>
-                <CartesianGrid strokeDasharray="3 3"/>
-                <XAxis dataKey="address"/>
-                <YAxis/>
-                <Tooltip/>
-                <Legend/>
-                <Bar onClick={(data) => {
-                    navigate("/single-server", {state: data.address});
-                }} name="Number of Packets Sent" barSize={30} dataKey="numPacketsSent" fill="var(--orange_wheel)"/>
-                <Bar onClick={(data) => {
-                    navigate("/single-server", {state: data.address});
-                }} name="Number of Packets Received" barSize={30} dataKey="numPacketsReceived"
-                     fill="var(--some_purple)"/>
-            </BarChart>
-            </div>
-            </>
+
+            <Collapsible  trigger={getGraphName()} transitionTime={100}>
+                <div style={{width: "100%", height: 600}}>
+                    <br/>
+            <ResponsiveContainer className="content">
+                <BarChart margin={{top: 30, right: 100, left: 0, bottom: 30}} height={500} width={1400} data={allPacketsPerIpClient}>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis dataKey="address"/>
+                    <YAxis/>
+                    <Tooltip/>
+                    <Legend/>
+                    <Bar onClick={(data) => {
+                        navigate("/single-server", {state: data.address});
+                    }} name="Number of Packets Sent" barSize={30} dataKey="numPacketsSent" fill="var(--orange_wheel)"/>
+                    <Bar onClick={(data) => {
+                        navigate("/single-server", {state: data.address});
+                    }} name="Number of Packets Received" barSize={30} dataKey="numPacketsReceived"
+                         fill="var(--some_purple)"
+                    />
+                </BarChart>
+            </ResponsiveContainer>
+                </div>
+            </Collapsible>
+            <br/>
+        </>
 
     }
 
