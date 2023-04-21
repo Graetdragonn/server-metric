0c94e7a63fde76c6d93f7387044006817e5d2f67
Grace <gekuntz@iastate.edu>
2023-04-21 09:04:45 -0500

    Got sent traffic data

--- front-end/src/components/time-graph/TimeGraph.tsx
+++ front-end/src/components/time-graph/TimeGraph.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import '../../style/Master.css';
 import { TimeGraph, getAllClientServers, getSentPacketCounts, organizeData} from "./TimeGraphLogic";
-//import { getAllSentTraffic } from "./TimeGraphLogic";
+import { getAllSentTraffic } from "./TimeGraphLogic";
 
 //https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
 
@@ -46,12 +46,21 @@ const LineGraph = () => {
                     {times: "3", "192.168.0.3": 9, "168.103.11.2": 8}];
 
     // get all servers
-    const getServerList = async () => {
+    const getServerTraffic = async () => {
         var servers = await getAllClientServers(email);
         set_servers_name(servers);
         console.log(servers);
+        var traffic_times = [];
+        for(let i = 0; i < servers.length; i++){
+           // const obj = {times: time[i], server1: values[i][j], server2: values[i][j+1]};
+            console.log("Serverlist[i]  " + servers[i]["address"].toString());
+            var traffic = getAllSentTraffic(servers[i]["address"]);
+            traffic_times.push(traffic);
+        }
+        console.log("TRAFFIC TIMES");
+        console.log(traffic_times)
     }
-    getServerList();
+    getServerTraffic();
     
 
 
--- front-end/src/requests/TrafficService.tsx
+++ front-end/src/requests/TrafficService.tsx
@@ -48,6 +48,28 @@ class TrafficService {
     }
 
     /**
+     * Get all traffic sent from a server
+     * @param server address
+     */
+    async getAllSentTrafficByServer(address: string) {
+        var res = "";
+        var config = {
+            method: 'get',
+            url: USER_API_BASE_URL + 'api/v1/traffic' + '/getAllSentTrafficByServer/' + address,
+            headers: {}
+        };
+
+        await axios(config)
+            .then(function (response: { data: any; }) {
+                res = JSON.stringify(response.data);
+            })
+            .catch(function (error: any) {
+                //alert(error);
+            });
+        return res;
+    }
+
+    /**
      * Get map of ports sent by address
      * @param address server address
      * @returns map of ports sent by address
