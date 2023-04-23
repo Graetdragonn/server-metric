46a74b496e2e82e0d5e5601862eefb3b2c135c65
Grace <gekuntz@iastate.edu>
2023-04-22 20:23:19 -0500

    Pushing before rebase

--- front-end/src/components/time-graph/TimeGraph.tsx
+++ front-end/src/components/time-graph/TimeGraph.tsx
@@ -1,71 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../../style/Master.css';
-import { TimeGraph, getAllClientServers, getSentPacketCounts} from "./TimeGraphLogic";
-import { getAllSentTraffic } from "./TimeGraphLogic";
+import { TimeGraph, getAllClientServers, getSentPacketCounts, organizeData} from "./TimeGraphLogic";
+//import { getAllSentTraffic } from "./TimeGraphLogic";
 
 //https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-
-/*
-type props = {
-    data: any[],
-    servers: string[]
-}
-*/
-
 
 
 const LineGraph = () => {
     // get user email
     const email = JSON.parse(localStorage.getItem('email') || '');
-   // var servers:any = [];
+    const[data, setData] = useState([] as any);
+    const[servers_name, set_servers_name] = useState([] as any);
 
     // Get all servers traffic
-    const getServerTraffic = async () => {
-        // Get servers
-        var servers = await getAllClientServers(email);
-        // Get traffic for each server
-        var total_dict:any = [];
-        var time_vals:any = [];
-        var ret:any = await getSentPacketCounts(servers, total_dict, time_vals);
-        total_dict = ret[0];
-        time_vals = ret[1];
-        // Ensure time data is in sorted order
-        time_vals.sort();
-        //console.log(time_vals);
-        console.log(total_dict);
-
-        // Sort the data by time and reformat it such that it follows below:
-        // {times: time, server1: packet_count, server2: packet_count... etc.}
-        /*
-        var data1:any = [];
-        for(let i = 0; i < time_vals.length; i++){
-            var dict_entry_string = "times: " + time_vals[i];
-            console.log("FIRST STR");
-            console.log(dict_entry_string);
-            for(let j = 0; j < servers.length; j++){
-                for(let k = 0; k < total_dict.length; k++){
-                    if((total_dict[k]["address"] == servers[j]) &&
-                        (time_vals[i] == total_dict[k]["time"])){
-                        dict_entry_string += ", " + servers[j] + ":" + total_dict[k]["count"];
-                    }else{
-                        dict_entry_string += ", " + servers[j] + ":0";
-                    }
-                }
+    //const getServerTraffic = async () => {
+    useEffect(() => {
+        async function getData(){
+            var globalServers:any = [];
+            // Get servers
+            var servers = await getAllClientServers(email);
+            for(let i = 0; i < servers.length; i++){
+                globalServers.push(servers[i]["address"]);
             }
-            console.log("DICT ENTRY STRING");
-            console.log(dict_entry_string);
+            set_servers_name(globalServers);
+            // Get traffic for each server
+            var total_dict:any = [];
+            var time_vals:any = [];
+            var ret:any = await getSentPacketCounts(servers, total_dict, time_vals);
+            total_dict = ret[0];
+            time_vals = ret[1];
+            // Ensure time data is in sorted order
+            time_vals.sort();
+
+            // Sort the data by time and reformat it such that it follows below:
+            // {times: time, server1: packet_count, server2: packet_count... etc.}
+            var data1 = await organizeData(time_vals, servers, total_dict, data);
+            setData(data1);
+            //console.log("DATAAAA")
+            //console.log(data1);
         }
-        */
-       //'{"content":{"path":"callie/circle","type":"file"},"video":{"videoId":"CvIr-2lMLs‌​k","endSeconds":"30","startSeconds":15}}'
-        var testStr = '{"times": "1", "192.23.11.3":5, "158.32.45.1":6}'
-        console.log(testStr);
-        var obj = JSON.parse(testStr);
-        console.log(obj);
-        //convertTimes(serv_traffic);
-    }
-    getServerTraffic();
+        getData();
+    }, []);
     
     // const obj = {times: time[i], server1: values[i][j], server2: values[i][j+1]};
+    /*
     function convertTime(unix_time: number){
         let millisec_time = new Date(unix_time * 1000);
         let hour_time = millisec_time.getHours();
@@ -75,18 +53,19 @@ const LineGraph = () => {
         console.log(time);
         return time;
     }
+    */
 
-    var data:any = [{times: "1", "192.168.0.3": 5, "168.103.11.2": 2},
+    var data2:any = [{times: "1", "192.168.0.3": 5, "168.103.11.2": 2},
                     {times: "2", "192.168.0.3": 8, "168.103.11.2": 3},
                     {times: "3", "192.168.0.3": 9, "168.103.11.2": 8}];
 
 
 
-    var servers = ["192.168.0.3", "168.103.11.2"];
-
+    var servers_name2 = ["192.168.0.3", "168.103.11.2"];
+   // var data3 = {data2, servers_name2}
     return (
         <div style={{marginTop: 30, marginBottom: 20}}>
-            <TimeGraph data={data} server_names={servers}/>
+            <TimeGraph data={data} server_names={servers_name}/>
         </div>
     );
 }
