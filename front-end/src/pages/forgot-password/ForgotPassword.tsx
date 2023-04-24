f458f8ec1866fca74ad0b6536471fc83034fc57d
megan3 <megan3@iastate.edu>
2023-04-24 18:24:13 -0500

    Update forgot password form to include email confirmation.

--- front-end/src/pages/forgot-password/ForgotPassword.tsx
+++ front-end/src/pages/forgot-password/ForgotPassword.tsx
@@ -17,19 +17,32 @@ export default function ForgotPasswordPage() {
 
   const [isValidEmail, setIsValidEmail] = useState(false);
 
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
   // to update user information when user inputs data
   const handleChange = (e: { target: { name: string; value: any; }; }) => {
     setEmail(e.target.value);
   };
 
-  // submits form
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // verifies fields and sends password reset email
+  // handles user input of email
+  const handleEmailSubmit = (email: string) => {
+    // verifies fields, lets user confirm if email is correct
     if (emailCheck(email) && !checkEmpty(email)) {
       setIsValidEmail(true);
     }
+  };
+
+  // handles user declining email displayed for confirmation
+  const handleIncorrectEmail = () => {
+    setIsValidEmail(false);
+    setIsConfirmed(false);
+  };
+
+  // submits form
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // send email here
+    setIsConfirmed(true);
   };
 
   return (
@@ -41,9 +54,14 @@ export default function ForgotPasswordPage() {
             <h1>Forgot Password</h1>
             <p style={{ fontSize: 15, textAlign: 'center' }}>Please submit your email to reset your password</p>
             <input placeholder='Email' type="text" required={true} name="email" value={email} onChange={handleChange}></input>
-            <button>Submit</button>
+            <button type="button" onClick={() => handleEmailSubmit(email)}>Submit</button>
           </form>
-          <form onSubmit={handleSubmit} style={{ display: isValidEmail ? '' : 'none' }}>
+          <form onSubmit={handleSubmit} style={{ display: (isValidEmail && !isConfirmed) ? '' : 'none' }}>
+            <p style={{ fontSize: 30, textAlign: 'center' }}>Is this email correct?: {email}</p>
+            <button type="submit">Yes</button>
+            <button type="button" onClick={() => handleIncorrectEmail()}>No</button>
+          </form>
+          <form onSubmit={handleSubmit} style={{ display: (isValidEmail && isConfirmed) ? '' : 'none' }}>
             <p style={{ fontSize: 50, textAlign: 'center' }}>An email was sent to reset your password</p>
             <button onClick={() => navigate(Constants.LOGIN_PAGE)}>Go to login</button>
           </form>
