1b68aff67b5b569d18c1fe20722a60b2b06cc610
Grace <gekuntz@iastate.edu>
2023-04-24 14:55:02 -0500

    Trying to get dependencies

--- front-end/src/components/world-map/WorldMapLogic.tsx
+++ front-end/src/components/world-map/WorldMapLogic.tsx
@@ -1,8 +1,21 @@
-import React, {useState, useEffect} from "react";
+import React from "react"; 
+import { ComposableMap, Geographies, Geography} from "react-simple-maps";
 
 
-export const WorldMap = () => {
+export const WorldMap = () => { 
 
+    return ( 
+    <div>
+        <ComposableMap>
+            <Geographies geography="/features.json">
+                {({ geographies:any }) =>
+                    geographies.map((geo:any) => (
+                        <Geography key={geo.rsmKey} geography={geo} />
+                    ))
+                }
+            </Geographies>
+        </ComposableMap>
+    </div>)
 }
 
 
