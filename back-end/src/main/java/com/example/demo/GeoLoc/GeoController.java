5d3cc760ecefe553c4d5936d49462e236362d47d
Casey Wong <cjwong@iastate.edu>
2023-04-25 20:31:54 -0500

    Geolocation of clients to specific server

--- back-end/src/main/java/com/example/demo/GeoLoc/GeoController.java
+++ back-end/src/main/java/com/example/demo/GeoLoc/GeoController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +24,16 @@ public class GeoController {
     }
 
     //GET request for a given IP address
-    @GetMapping
-    public Geo getGeo(@RequestParam String ip) throws IOException, GeoIp2Exception {
+    @GetMapping("ip/{ip}")
+    @CrossOrigin
+    public Geo getGeo(@PathVariable("ip") String ip) throws IOException, GeoIp2Exception {
         return geoService.getGeo(ip);
+    }
+
+    //GET request for list of client IPs related to the server
+    @GetMapping("serverip/{ip}")
+    @CrossOrigin
+    public Geo[][] getServerGeos(@PathVariable("ip") String ip) throws IOException, GeoIp2Exception {
+        return geoService.getServerGeos(ip);
     }
 }
