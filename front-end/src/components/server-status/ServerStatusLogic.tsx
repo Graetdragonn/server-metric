018ea2f478f1dec98ee6c5b1f17395374d7d71c0
megan3 <megan3@iastate.edu>
2023-04-27 16:35:03 -0500

    Committing work to save progress while server is down.

--- front-end/src/components/server-status/ServerStatusLogic.tsx
+++ front-end/src/components/server-status/ServerStatusLogic.tsx
@@ -15,6 +15,24 @@ export async function getUserServers(email: string) {
     return servers;
 }
 
+export function getServersFromSubnet(servers: any[], subnet: string) {
+    servers.forEach((subnetAndFullIP: {firstThree: string, addresses: [address: string]}) => {
+        if (subnetAndFullIP["firstThree"] === subnet) {
+            console.log("TO RETURN: ", subnetAndFullIP["addresses"]);
+            return subnetAndFullIP["addresses"];
+        }
+    });
+    console.log("HERE");
+    return [];
+}
+
+
+/**
+ * Returns name (if available) and subnet (taken from TimeGraph.tsx, function not in logic file)
+ * @param clientName client first and last name
+ * @param subnetAddress 
+ * @returns string with name (if available) and subnet address
+ */
 export function getName(clientName: string, subnetAddress: string){
     if (clientName != "") {
         return clientName + ", Subnet: " + subnetAddress
@@ -23,8 +41,20 @@ export function getName(clientName: string, subnetAddress: string){
     }
 }
 
+export function renderServerList(serverList: any[], subnetAddress: string) {
+    var returning = [] as any[];
+    // serverList.forEach((subnetAndFullIP: {firstThree: string, addresses: string[]}, index: number) => {
+    //     if (subnetAndFullIP["firstThree"] === subnetAddress) {
+    //         subnetAndFullIP["addresses"].map((address: string) => {
+    //             return(
+    //                 returning.push(<div key={index}>Server {address}: {checkServerStatus(address)}</div>)
+    //             )
+    //         });
+    //     }
+    // });
+    return returning;
+};
 
-// // if statement for checking if address goes in subnet
-// if(getSubnetFromFullAddress(clientAndServersAndPackets[i]["servers"][j]) == subnetAddress){
-//     returnList.push({ address: clientAndServersAndPackets[i]["servers"][j], sentPackets: clientAndServersAndPackets[i]["sentPackets"][j], receivedPackets: clientAndServersAndPackets[i]["receivedPackets"][j]})
-// }
+function checkServerStatus(address: string) {
+    return "UP";
+}
